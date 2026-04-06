c19f5d2240b2a55dee21cea3cc8d74d2c3cf2f4c
CAUA DOS SANTOS FELIX <00001093455433sp@aluno.educacao.sp.gov.br>
2026-04-06 17:30:01 -0300

    Ativadade Consultas no JavaScript (DOM)

---
+++ View/script.js
@@ -0,0 +1,6 @@
+let paragrafo = document.getElementById("meu-paragrafo");
+let botao = document.getElementById("meu-botao");
+
+botao.addEventListener("click", function() {
+    paragrafo.textContent = "Amor❤️!";
+});
